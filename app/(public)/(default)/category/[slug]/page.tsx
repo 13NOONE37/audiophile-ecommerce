87777b4a20bea679cc5d72a73b06804a3752573d
13NOONE37 <oliwerklauze22@gmail.com>
2026-03-01 18:07:21 +0100

    category page almost done; new product need to be ifed

--- app/(public)/(default)/category/[slug]/page.tsx
+++ app/(public)/(default)/category/[slug]/page.tsx
@@ -1,9 +1,12 @@
+import { Button, LinkButton } from '@/components/button';
 import { db } from '@/db/db';
 import { categories, productImages, products } from '@/db/schema';
 import { getCategoryIdTag } from '@/features/categories/db/cache';
 import { getCategoryIdProductsTag } from '@/features/products/db/cache';
-import { eq } from 'drizzle-orm';
+import { cn } from '@/lib/utils';
+import { and, eq } from 'drizzle-orm';
 import { cacheTag } from 'next/cache';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
@@ -27,18 +30,70 @@ export default async function CategoryPage({
   }
 
   const products = await getProductsForCategory(category.id);
-
+  //TODO add new until functionality (we need to modify database again)
+  //TODO add blur field in images but we will be pasting it to database not seperate file a lot of easier to handle
   return (
-    <div>
-      <h1 className='bg-body w-full text-heading-2 text-body-inverted uppercase'>
-        {category.name}
-      </h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className='w-full grid place-items-center bg-body py-8 md:pt-21 md:pb-24.5 lg:py-24.5'>
+        <h1 className='heading-2 text-body-inverted uppercase'>
+          {category.name}
+        </h1>
+      </div>
+      <section className='max-w-(--max-width) mx-auto px-6 md:px-10 mt-16 md:mt-30 lg:mt-40 box-content'>
+        <ul className='flex flex-col gap-30 lg:gap-40'>
+          {products.map((product, index) => (
+            <li
+              key={product.id}
+              className={cn(
+                'flex flex-col items-center gap-8 md:gap-13 lg:gap-31',
+                index % 2 == 0 ? 'lg:flex-row' : 'lg:flex-row-reverse',
+              )}
+            >
+              <div className='relative w-full lg:w-[540px] aspect-654/704 md:aspect-[1378/704] lg:aspect-[1080/1120] rounded-[8px] overflow-hidden'>
+                <picture>
+                  <source
+                    media='(max-width: 768px)'
+                    srcSet='/images/products/zx7-speaker/default/mobile/image-category-page-preview.jpg'
+                  />
+                  <source
+                    media='(max-width: 1024px)'
+                    srcSet='/images/products/zx7-speaker/default/tablet/image-category-page-preview.jpg'
+                  />
+
+                  <Image
+                    src='/images/products/zx7-speaker/default/desktop/image-category-page-preview.jpg'
+                    alt={''}
+                    fill
+                    // placeholder='blur'
+                    // blurDataURL='/images/home/bestProducts/zx9-speaker/blur.jpg'
+                    priority
+                    className='object-cover'
+                  />
+                </picture>
+              </div>
+              <div className='flex flex-col items-center lg:items-start'>
+                <span className='overline-text text-brand-primary font-normal uppercase'>
+                  New product
+                </span>
+                <h2 className='heading-2 text-body uppercase mt-6 md:mt-4'>
+                  {product.name}
+                </h2>
+                <p className='content-text text-body text-center lg:text-start max-w-[60ch] mt-6 md:mt-8'>
+                  {product.description}
+                </p>
+                <LinkButton
+                  variant='primary'
+                  href={`/product/${product.slug}`}
+                  className='uppercase mt-6 lg:mt-10'
+                >
+                  See product
+                </LinkButton>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
 
@@ -66,7 +121,10 @@ async function getProductsForCategory(categoryId: string) {
       name: true,
       description: true,
     },
-    where: eq(products.categoryId, categoryId),
+    where: and(
+      eq(products.categoryId, categoryId),
+      eq(products.is_active, true),
+    ),
     with: {
       images: {
         where: eq(productImages.role, 'main'), //Only main photos
@@ -76,6 +134,7 @@ async function getProductsForCategory(categoryId: string) {
         },
       },
     },
+    orderBy: (products, { asc }) => [asc(products.createdAt)],
   });
 
   return productsList;
--- app/(public)/(default)/layout.tsx
+++ app/(public)/(default)/layout.tsx
@@ -1,4 +1,6 @@
+import Categories from '../_components/categories';
 import Header from '../_components/header';
+import Outro from '../_components/outro';
 export default function PublicDefaultLayout({
   children,
 }: {
@@ -7,8 +9,17 @@ export default function PublicDefaultLayout({
   return (
     <>
       <Header />
-      <main className='max-w-(--max-width) mx-auto px-6 md:px-10 box-content'>
+      <main>
         {children}
+
+        <div className='max-w-(--max-width) mx-auto px-6 md:px-10 box-content'>
+          <div className='mt-44.5 md:mt-43 lg:mt-60'>
+            <Categories />
+          </div>
+          <div className='mb-30 md:mb-24 lg:mb-50 mt-24 md:mt-30 lg:mt-50'>
+            <Outro />
+          </div>
+        </div>
       </main>
     </>
   );
